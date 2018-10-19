5fa7bbddc5b65d187e21f59d2107f4487a8a8e8d
seungkyu2 <zx6658@naver.com>
2018-10-19 23:17:02 +0900

    feat: text 변경

--- src/components/Word.js
+++ src/components/Word.js
@@ -24,28 +24,21 @@ class Word extends React.Component {
     const { number } = this.state;
     const { text } = this.props;
     return (
-      <div className="slideshow">
+      <React.Fragment>
         <a className="button close" onClick={this.props.onClose}>
-          <svg
-            fill="#fff"
-            height="40"
-            width="40"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg">
-            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
-            <path d="M0 0h24v24H0z" fill="none" />
-          </svg>
+        다른 질문하기
         </a>
-        <div className="slide slide--current">
-          <div className={`slide__bg slide__bg--1`} />
-          <h2 className={`word word--${number} word--question`}>Q) {text}</h2>
-          {/* 일단은 더미데이터로 예상 질문들 만들어보기 */}
-          {text =='병원이 어디예요' && <div className={`word word--answer`}>A) 서울시 서초구 강남동입니다</div>}
-          {(text.indexOf('고장') > 0) && <div className={`word word--answer`}>A) 접수되었습니다. </div>}
-          {(text.indexOf('중환자실') >= 0) && (text.indexOf('층') >= 0) && <div className={`word word--answer`}> 3층입니다. </div>}
+        <div className="slideshow">
+          <div className="slide slide--current">
+            <div className={`slide__bg slide__bg--1`} />
+            <h2 className={`word word--${number} word--question`}>Q) {text}</h2>
+            {/* 일단은 더미데이터로 예상 질문들 만들어보기 */}
+            {text =='병원이 어디예요' && <div className={`word word--answer`}>A) 서울시 서초구 강남동입니다</div>}
+            {(text.indexOf('고장') > 0) && <div className={`word word--answer`}>A) 접수되었습니다. </div>}
+            {(text.indexOf('중환자실') >= 0) && (text.indexOf('층') >= 0) && <div className={`word word--answer`}> 3층입니다. </div>}
+          </div>
         </div>
-        
-      </div>
+      </React.Fragment>
     );
   }
 }
