1ee40690dd844e898380ef052b32d24e5cc44be6
seungkyu2 <zx6658@naver.com>
2018-10-24 22:19:36 +0900

    feat: 입원교육 영상

--- src/components/Word.js
+++ src/components/Word.js
@@ -36,6 +36,8 @@ class Word extends React.Component {
             {text =='병원이 어디예요' && <div className={`word word--answer`}>A) 서울시 서초구 강남동입니다</div>}
             {(text.indexOf('고장') > 0) && <div className={`word word--answer`}>A) 접수되었습니다. </div>}
             {(text.indexOf('중환자실') >= 0) && (text.indexOf('층') >= 0) && <div className={`word word--answer`}> 3층입니다. </div>}
+            {(text.indexOf('입원') >= 0) && (text.indexOf('교육') >= 0) && 
+              <iframe allowfullscreen className={`hospitalInfo--vedio`} width="912" height="513" src="https://www.youtube.com/embed/TwOB2OiqlDg?&amp;autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>}
           </div>
         </div>
       </React.Fragment>
