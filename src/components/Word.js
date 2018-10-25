b25611d1970cdded7f8ee81706796f58fdd93f5a
seungkyu2 <zx6658@naver.com>
2018-10-26 00:02:48 +0900

    fix: fix bug with keyword 고장

--- src/components/Word.js
+++ src/components/Word.js
@@ -34,7 +34,7 @@ class Word extends React.Component {
             <h2 className={`word word--${number} word--question`}>Q) {text}</h2>
             {/* 일단은 더미데이터로 예상 질문들 만들어보기 */}
             {text =='병원이 어디예요' && <div className={`word word--answer`}>A) 서울시 서초구 강남동입니다</div>}
-            {(text.indexOf('고장') > 0) && <div className={`word word--answer`}>A) 접수되었습니다. </div>}
+            {(text.indexOf('고장') >= 0) && <div className={`word word--answer`}>A) 접수되었습니다. </div>}
             {(text.indexOf('중환자실') >= 0) && (text.indexOf('층') >= 0) && <div className={`word word--answer`}> 3층입니다. </div>}
             {(text.indexOf('입원') >= 0) && (text.indexOf('교육') >= 0) && 
               <iframe allowfullscreen className={`hospitalInfo--vedio`} width="912" height="513" src="https://www.youtube.com/embed/TwOB2OiqlDg?&amp;autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>}
